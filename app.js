9e5b44fcd376a41cf0165b06942e5ffc3a716f1b
Oleksandr Chyhryn <as.chigrin@gmail.com>
2023-05-13 14:57:16 +0300

    add response headers

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res
+    .setHeader("Access-Control-Allow-Origin", "*")
+    .setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+    .setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
